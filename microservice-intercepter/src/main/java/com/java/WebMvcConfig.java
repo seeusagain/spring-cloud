827e8edb0706327d42716a2c8eb62a7e1222cf41
lu.xu <765110919@qq.com>
2018-07-23 16:05:11 +0800

    拦截器添加对swaggerUi的拦截控制

--- microservice-intercepter/src/main/java/com/java/WebMvcConfig.java
+++ microservice-intercepter/src/main/java/com/java/WebMvcConfig.java
@@ -3,8 +3,10 @@ package com.java;
 import com.java.test.interceptor.AuthenticationInterceptor;
 import com.java.test.interceptor.SessionInterceptor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
@@ -17,12 +19,33 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
     @Value("${server.context-path}")
     private String contextPath;
     
+    /**
+     * 使用@Bean可以在拦截器中进行注入操作
+     * @return
+     */
+    @Bean
+    AuthenticationInterceptor authenticationInterceptor() {
+        return new AuthenticationInterceptor();
+    }
+    
+    @Bean
+    SessionInterceptor sessionInterceptor() {
+        return new SessionInterceptor(contextPath);
+    }
+    
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        /**
-         * 会话拦截器
-         */
-        registry.addInterceptor(new SessionInterceptor(contextPath));
-        registry.addInterceptor(new AuthenticationInterceptor());
+        registry.addInterceptor(authenticationInterceptor());
+        registry.addInterceptor(authenticationInterceptor())
+            /** 会话拦截器, 需要考虑对swaggerUi的影响*/
+            .addPathPatterns("/**")
+            .excludePathPatterns("/user/login")
+            .excludePathPatterns("/swagger-resources/**", "/webjars/**", "/v2/**", "/swagger-ui.html/**");
+    }
+    
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
